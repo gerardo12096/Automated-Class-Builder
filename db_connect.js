594f9ba38cc97cd93596c2221aabe5120ac8f1c1
Rutvik Vachhani <rutvik.vachhani@yahoo.com>
2022-01-06 21:54:16 +0530

    update to db connect from connect to pool

--- db_connect.js
+++ db_connect.js
@@ -1,17 +1,12 @@
 //Connect to database
 var mysql = require("mysql");
 
-var con = mysql.createConnection({
+var con =  mysql.createPool({
   host: "us-cdbr-east-05.cleardb.net",
   user: "b14065fc30e286",
   password: "68d7aec9",
   database: "heroku_6ee4a0fcdd21b51",
   multipleStatements: true,
-});
-
-con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
 });
 
 module.exports = con;
--- reqpdf.js
+++ reqpdf.js
@@ -6,11 +6,12 @@ var db_connect = require("./db_connect");
 
 var prereqChain1 = ["MATH 150A", "MATH 150B", "MATH 262", "MATH 482"];
 var lastcourse = 0;
+
 for (var i = 0; i < prereqChain1.length; i++) {
   var Cno = prereqChain1[i].substring(5, prereqChain1[i].length);
   // console.log(Cno);
   let sqll =
-    "SELECT * FROM practice.CourseCompletedByStudent WHERE StudentId = '200507859' AND Department = 'MATH' AND CourseNumber = '" +
+    "SELECT * FROM CourseCompletedByStudent WHERE StudentId = '200507859' AND Department = 'MATH' AND CourseNumber = '" +
     Cno +
     "';";
   db_connect.query(sqll, (err, result) => {
@@ -20,27 +21,17 @@ for (var i = 0; i < prereqChain1.length; i++) {
     }
     //console.log(result);
     if (!result[0]) {
-      console.log("hi");
-      if (lastcourse == 0) {
-        lastcourse = i;
-      }
-      // console.log(lastcourse);
-    } else {
-      if (
-        (result[0].Grade =
-          "A" || "A-" || "B+" || "B" || "B-" || "C+" || "C" || "IP")
-      ) {
-        console.log(i);
-        console.log(result[0].CourseNumber);
+      console.log("next course required = " + prereqChain1[lastcourse]);
+      } else {
+      if (result[0].Grade = "A" || "A-" || "B+" || "B" || "B-" || "C+" || "C" || "IP") {
+        //console.log(i);
+        console.log(result[0].CourseNumber + " passed");
         // console.log("Below is Cno");
         // console.log(Cno);
         i++;
+        lastcourse++;
       }
     }
   });
-  // return lastcourse;
 }
 db_connect.end();
-console.log("outside");
-console.log(lastcourse);
-//console.log(prereqChain1[lastCourse + 1]);
