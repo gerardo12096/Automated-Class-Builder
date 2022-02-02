f89ac30e89d7139e2573b908a395ea0a342b612f
Rutvik Vachhani <rutvik.vachhani@yahoo.com>
2022-02-02 14:49:24 -0800

    prereq.js changes

--- public/assets/prereq.js
+++ public/assets/prereq.js
@@ -18,34 +18,25 @@ for (var i = 0; i < obj.length; i++) {
 	//this loop splits all AND in an array and OR's in other array
 	for (var j = 0; j < eachCourse.length; j++) {
 		var and = eachCourse[j].split("+");
-		console.log(and);
-		if(j != eachCourse.length - 1)
-			console.log("OR");
+		//console.log(and);
 
 		//this loop is not functional right now, this loop should find all the courses in the array in the database to confirm whether student have taken and passed the courses and is eligible for the course.
 		for (var check = 0; check < and.length; check++) {
-			let sql = "SELECT courseNumber, grade FROM CourseCompletedByStudent WHERE Department = '" + and[check].substr(0, and[check].indexOf(' ')) + "' AND CourseNumber = '" + and[check].substr(and[check].indexOf(' ') + 1) + "';";
-			function checkGrades() {
-				return new Promise((resolve, reject) => {
-					db_connect.query(sql, function (err, data) {
-						if (err) {
-							return reject(err);
-						}
-						resolve(data);
-					});
-				})
-			}
+			var coursename = and[check].substr(0, and[check].indexOf(' '));
+			var courseno = and[check].substr(and[check].indexOf(' ') + 1);
 
-			async (req, res) => {
-				try {
-					const checking = await checkGrades();
-					res.send(checking);
-					console.log(checking);
-				} catch (error) {
-					res.status(500).send(error)
+			console.log(coursename, courseno);
+
+			let sql = "SELECT courseNumber, grade FROM CourseCompletedByStudent WHERE Department = '" + coursename + "' AND CourseNumber = '" + courseno + "';";
+
+			/*db_connect.query(sql, (err, result) => {
+				if(result){
+					console.table(result);
 				}
-			}
+			})*/
 		}
+		if(j != eachCourse.length - 1)
+			console.log("OR");
 	}
 	console.log('');
 }
